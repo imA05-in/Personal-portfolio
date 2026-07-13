dbdb44167b860a1e0a4b032c398c16f9f520aac2
Abhishek <abhishek29112005@gmail.com>
2026-07-13 16:38:40 +0530

    add Bio Component in Home Page

--- src/components/PersonalBio/PersonalBio.jsx
+++ src/components/PersonalBio/PersonalBio.jsx
@@ -1,7 +1,28 @@
-export default function PersonalBio(){
-    return(
-        <div className="h-100 border">
-            PersonalBio
-        </div>
-    )
+import Profile from "../../assets/images/Profile.png";
+
+export default function PersonalBio({
+  img = Profile,
+  heading,
+  introPrimary,
+  introSecondary,
+}) {
+  return (
+    <div className="w-full px-2 py-1 flex flex-col gap-4">
+      {/* Profile Image */}
+      <div className="rounded-full">
+        <img
+          src={img}
+          alt="img"
+          className="rounded-full object-cover opacity-90 bg-white size-20"
+        />
+      </div>
+      {/* heading  */}
+      <div className="font-extrabold text-2xl">{heading}</div>
+      {/* intro */}
+      <div className="font-medium flex flex-col gap-2 text-justify">
+        <div className="flex ">{introPrimary}</div>
+        <div>{introSecondary}</div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import {PersonalBio} from "../components/index"
+
 export default function Home(){
+    
     return(
-        <div className="flex justify-center ">Home</div>
+        <div className="flex justify-center ">
+            <PersonalBio heading={`Hi, I'm Abhishek Singh!`} introPrimary={`I’m a frontend developer and bit of a  designer. I’m passionate about building modern and cool looking web sites, specializing in modern framework like react and surrounding frontend technologies like redux for state management, react-router for client-side routing, react-hook-form, tailwindCSS, HTML, Js, and Figma.`}
+            introSecondary={`When im not busy i like to read books and learn new stuff. `}
+            />
+        </div>
     )
 }
