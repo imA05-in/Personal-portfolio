2dacbab86ddf7d1674dbc954d221591daa6ab858
Abhishek <abhishek29112005@gmail.com>
2026-07-13 13:45:18 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import {Button, Content, Footer, Hero, ImageBlock, NavBar, PersonalBio, ProjectCard, SkillList,} from "./components/index";
+import {Outlet} from "react-router"
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen items-center ">
+      <NavBar/>
+      <main className="flex-1 max-w-7xl">
+        <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
