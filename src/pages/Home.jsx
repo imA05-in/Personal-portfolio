8a02847daa7a18b5ea7ef2ac4d8418d82e563adc
Abhishek <abhishek29112005@gmail.com>
2026-07-13 16:41:29 +0530

    format

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import {PersonalBio} from "../components/index"
+import { PersonalBio } from "../components/index";
 
-export default function Home(){
-    
-    return(
-        <div className="flex justify-center ">
-            <PersonalBio heading={`Hi, I'm Abhishek Singh!`} introPrimary={`I’m a frontend developer and bit of a  designer. I’m passionate about building modern and cool looking web sites, specializing in modern framework like react and surrounding frontend technologies like redux for state management, react-router for client-side routing, react-hook-form, tailwindCSS, HTML, Js, and Figma.`}
-            introSecondary={`When im not busy i like to read books and learn new stuff. `}
-            />
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className="flex justify-center ">
+      <PersonalBio
+        heading={`Hi, I'm Abhishek Singh!`}
+        introPrimary={`I’m a frontend developer and bit of a  designer. I’m passionate about building modern and cool looking web sites, specializing in modern framework like react and surrounding frontend technologies like redux for state management, react-router for client-side routing, react-hook-form, tailwindCSS, HTML, Js, and Figma.`}
+        introSecondary={`When im not busy i like to read books and learn new stuff. `}
+      />
+    </div>
+  );
 }
