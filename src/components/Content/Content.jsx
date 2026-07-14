624c2c6f1efb949b007bb58e34463002c044560a
Abhishek <abhishek29112005@gmail.com>
2026-07-14 15:18:57 +0530

    Refactor Content component to include PersonalBio and ProjectCard

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,7 +1,31 @@
+import {PersonalBio,ProjectCard} from "../index"
+import useGitData from "../../hooks/useGitData"
+
+
 export default function Content(){
-    return(
-        <div>
-            Content
-        </div>
-    )
+    const data = useGitData({ user: "imA05-in" })
+
+  return (
+    <div className="flex flex-col gap-10 w-full px-2">
+      <PersonalBio
+        heading={`Hi, I'm Abhishek Singh!`}
+        introPrimary={`I’m a frontend developer and bit of a  designer. I’m passionate about building modern and cool looking web sites, specializing in modern framework like react and surrounding frontend technologies like redux for state management, react-router for client-side routing, react-hook-form, tailwindCSS, HTML, Js, and Figma.`}
+        introSecondary={`When im not busy i like to read books and learn new stuff. `}
+      />
+
+      {/* work */}
+      <div className="flex flex-col gap-10 ">
+        <h2 className="font-medium text-xl">Work</h2>
+        {data.map((item) => (
+          <div key={item.name} className="pt-6">  {/* filter unwanted projects */}
+            <ProjectCard
+              title={item.name}
+              btnLable={item.name}
+              description={item.description}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
