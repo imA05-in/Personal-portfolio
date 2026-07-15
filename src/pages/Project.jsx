4f22022218c14658532671c35ec730e336f02786
Abhishek <abhishek29112005@gmail.com>
2026-07-15 14:51:03 +0530

    add Hero to Project Component and provide data

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,15 +1,22 @@
-import { useParams } from "react-router"
-import { useSelector } from "react-redux"
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+import { Hero } from "../components/index";
 
-export default function Project(){
-    const slug = useParams()
-    const data = useSelector((state)=>state.gitdata.repos.filter((item)=> item.name === Object.values(slug)[0]))
-    console.log(data);
-    
-    
-    return(
-        <div>
-            Project: {data[0].name}
-        </div>
-    )
+export default function Project() {
+  const slug = useParams();
+  const data = useSelector((state) =>
+    state.gitdata.repos.filter((item) => item.name === Object.values(slug)[0]),
+  );
+  const ProjData = data[0];
+
+  const title = ProjData.name;
+  const img = `https://raw.githubusercontent.com/imA05-in/${title}/main/screenshots/0.png`;
+
+  return (
+    <div className="px-2 w-full">
+      <div>
+        <Hero text={title} Himg={img} />
+      </div>
+    </div>
+  );
 }
