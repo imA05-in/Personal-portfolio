718c99dc1e0133c38098c9c90b270b79b1917306
Abhishek <abhishek29112005@gmail.com>
2026-07-15 15:21:14 +0530

    add GithubLogo Component to ProjectCard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,12 +1,15 @@
-import { Button } from "../index";
+import { Button,GithubBtn } from "../index";
 import ProjThmbn from "../../assets/images/projThumbnail.png";
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router";
+import githubLogo from "../../assets/images/githubLogo.png"
+
 export default function ProjectCard({
   thumbnail = ProjThmbn,
   title = "Title",
   description = "Lorem",
   btnLable,
+  gitLink,
 }) {
 
   return (
@@ -16,16 +19,27 @@ export default function ProjectCard({
         <img src={`https://raw.githubusercontent.com/imA05-in/${title}/main/screenshots/0.png`} alt="" className="rounded" />
       </div>
       {/* desciption */}
-      <div className="flex flex-col justify-between py-2 md:px-4 gap-2">
+      <div className="flex flex-col justify-between py-2 md:px-4 gap-2 w-full">
         {/* title */}
         <div className="font-medium text-lg">{title}</div>
         {/* description */}
         <div className="project description">{description}</div>
+      
+        <div className="flex items-center justify-between gap-10 ">
         <NavLink
         to={`/${title}`}
         >
           <Button label={btnLable} classname="" />
         </NavLink>
+        <div>
+          <GithubBtn 
+          alt={`github`}
+          src={githubLogo}
+          link={gitLink}
+          
+          />
+        </div>
+          </div>
       </div>
     </div>
   );
