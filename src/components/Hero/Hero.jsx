4842693631a0b6f952713f011f39f218b3bbebdb
Abhishek <abhishek29112005@gmail.com>
2026-07-15 14:50:34 +0530

    add Hero UI

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,16 @@
-export default function Hero(){
-    return(
-        <div>
-            Hero
-        </div>
-    )
+import Heroimg from "../../../screenshots/0.png";
+
+export default function Hero({ Himg = Heroimg, text="Hero text" }) {
+  return (
+    <div className="relative">
+      <img
+        src={Himg}
+        alt="img"
+        className=" max-h-70 object-cover opacity-10 w-full bg-black"
+      />
+      <div className="absolute top-0 flex justify-center items-center w-full h-full ">
+        <p className="md:text-6xl text-3xl font-bold opacity-80">{text}</p>
+      </div>
+    </div>
+  );
 }
