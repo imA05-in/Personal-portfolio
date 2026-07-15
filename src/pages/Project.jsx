6bdc362cc47cba1b7f9766ff1c0861036ecb8140
Abhishek <abhishek29112005@gmail.com>
2026-07-15 14:00:13 +0530

    update Project component to recieve project from url

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,15 @@
+import { useParams } from "react-router"
+import { useSelector } from "react-redux"
+
 export default function Project(){
+    const slug = useParams()
+    const data = useSelector((state)=>state.gitdata.repos.filter((item)=> item.name === Object.values(slug)[0]))
+    console.log(data);
+    
+    
     return(
         <div>
-            Project
+            Project: {data[0].name}
         </div>
     )
 }
