71aa7059612598f9fbdec999d1e6deedf0f9804c
Abhishek <abhishek29112005@gmail.com>
2026-07-15 14:51:25 +0530

    UI update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen items-center bg-[#FFFDFB]">
       <NavBar/>
-      <main className="flex-1 max-w-7xl w-full ">
+      <main className="flex-1 flex flex-col items-center w-full ">
         <Outlet/>
       </main>
       <Footer/>
